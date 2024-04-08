784f11bd4e71c54fe28e6cd48bbfc5c4c3f16ab8
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-04-08 19:41:41 +0700

    feat(MainActivity): create main activity.

---
+++ Project/Android/app/src/main/java/com/singularityindonesia/singularityindonesia/MainActivity.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Stefanus Ayudha (stefanus.ayudha@gmail.com)
+ * Created on 04/03/2024 12:00
+ * You are not allowed to remove the copyright.
+ */
+package com.singularityindonesia.singularityindonesia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.singularityindonesia.composeapp.ExampleApp
+import com.singularityindonesia.composeapp.MainApp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainApp()
+        }
+    }
+}
